b1a90eb74c462f6f2e5d737309940d135ac76196
phoebeliu123 <phoebeliu1006@gmail.com>
2023-10-25 12:36:22 +0800

    modify apply random force

--- vrx_gazebo/scripts/apply_model_force.py
+++ vrx_gazebo/scripts/apply_model_force.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import rospy
+import random
+import time
 from gazebo_msgs.srv import ApplyBodyWrench
 from geometry_msgs.msg import Wrench, Point, Vector3
 
@@ -13,8 +15,14 @@ class ApplyForceNode:
 
         self.model_name = rospy.get_param("~model_name", "wamv")
         self.link_name = rospy.get_param("~link_name", "wamv/base_link")
+        self.dir = [1, -1]
         self.force_x = rospy.get_param("~force_x", 0.0)
         self.force_y = rospy.get_param("~force_y", 0.0)
+        self.random_force = rospy.get_param("~randomForce", False)
+        self.last_time = time.time()
+        if(self.random_force):
+            self.force_x = random.uniform(10.0, 13.0)*random.choice(self.dir)
+            self.force_y = random.uniform(10.0, 13.0)*random.choice(self.dir)
 
     def run(self):
         try:
@@ -24,6 +32,12 @@ class ApplyForceNode:
             force.torque = Vector3(x=0.0, y=0.0, z=0.0)
 
             while not rospy.is_shutdown():
+                if self.random_force and (time.time() - self.last_time) > 10.0:
+                    self.force_x = random.uniform(10.0, 13.0)*random.choice(self.dir)
+                    self.force_y = random.uniform(10.0, 13.0)*random.choice(self.dir)
+                    force.force = Vector3(x=self.force_x, y=self.force_y, z=0.0)
+                    force.torque = Vector3(x=0.0, y=0.0, z=0.0)
+                    self.last_time = time.time()
                 body_name_str = self.model_name + "::" + self.link_name
                 self.apply_wrench(body_name = body_name_str,
                                   reference_frame = 'world', 
@@ -31,6 +45,7 @@ class ApplyForceNode:
                                   wrench = force,
                                   start_time = rospy.Time(0.0),
                                   duration = rospy.Duration(1.0))
+                print(f"force_x: {self.force_x}, force_y: {self.force_y}")
                 self.rate.sleep()
 
         except ValueError:
