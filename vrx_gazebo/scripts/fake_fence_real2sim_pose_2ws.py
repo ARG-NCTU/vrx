9160a7a18c8fba3ae117789781edfa71b765cafe
yutingk <yutingk.ee11@nycu.edu.tw>
2023-12-16 18:20:31 +0800

    fix real2sim pose

--- vrx_gazebo/scripts/fake_fence_real2sim_pose_2ws.py
+++ vrx_gazebo/scripts/fake_fence_real2sim_pose_2ws.py
@@ -16,11 +16,11 @@ class RealtoSimTransform:
         # self.model_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, self.model_callback)
         self.pub_set_model_state = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=1)
         self.pub_pose = rospy.Publisher("/fake_fence_real2sim", PoseStamped, queue_size=1)
-        self.sub_wamv_pose = rospy.Subscriber("/wamv/truth_map_posestamped", PoseStamped, self.wamv_pose_callback)
-        self.sub_wamv2_pose = rospy.Subscriber("/wamv2/truth_map_posestamped", PoseStamped, self.wamv2_pose_callback)
+        self.sub_wamv_pose = rospy.Subscriber("/gazebo/wamv/pose", PoseStamped, self.wamv_pose_callback)
+        self.sub_wamv2_pose = rospy.Subscriber("/gazebo/wamv2/pose", PoseStamped, self.wamv2_pose_callback)
         self.sub_joy = rospy.Subscriber("joy", Joy, self.joy_callback)
         self.sync_freq = rospy.get_param("sync_freq", 20)
-        self.timer_set_model_state = rospy.Timer(rospy.Duration(0.05), self.timer_callback)
+        self.timer_set_model_state = rospy.Timer(rospy.Duration(0.05), self.timer_callback)   
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         self.tf_msg = TransformStamped()
         
@@ -107,30 +107,30 @@ class RealtoSimTransform:
                 ]
             )
             q = tf_trans.quaternion_from_euler(0, 0, euler[2])
-            pose_wamv2_to_map.pose.orientation.x = self.wamv_pose.pose.orientation.x #q[0]
-            pose_wamv2_to_map.pose.orientation.y = self.wamv_pose.pose.orientation.y #q[1]
+            pose_wamv2_to_map.pose.orientation.x = q[0] #self.wamv_pose.pose.orientation.x 
+            pose_wamv2_to_map.pose.orientation.y = q[1] #self.wamv_pose.pose.orientation.y 
             pose_wamv2_to_map.pose.orientation.z = q[2]
             pose_wamv2_to_map.pose.orientation.w = q[3]
             
             
-            self.tf_msg.header.stamp = rospy.Time.now()
+            self.tf_msg.header.stamp = self.time
             self.tf_msg.header.frame_id = "map"
             self.tf_msg.child_frame_id = "wamv2/base_link"
             self.tf_msg.transform.translation = pose_wamv2_to_map.pose.position
             self.tf_msg.transform.rotation = pose_wamv2_to_map.pose.orientation
-            self.tf_broadcaster.sendTransform(self.tf_msg) 
+            self.tf_broadcaster.sendTransform(self.tf_msg)
+            
             
             self.pub_pose.publish(pose_wamv2_to_map)
             print("wamv2 pose to map: ", pose_wamv2_to_map)
+            self.set_model(model_name = "wamv2", pose = pose_wamv2_to_map)    
             
             # # update wamv2 pose every 1/sync_freq seconds
-            if rospy.Time.now() - self.time > rospy.Duration(1/self.sync_freq):
-                self.time = rospy.Time.now()  
-                print("update wamv2 pose")
-                self.set_model(model_name = "wamv2", pose = pose_wamv2_to_map)  
-                
-            # self.set_model(model_name = "wamv2", pose = pose_wamv2_to_map)   
-            # print("wamv2 pose to map: ", pose_wamv2_to_map)
+            # if rospy.Time.now() - self.time > rospy.Duration(1/self.sync_freq):
+            #     self.time = rospy.Time.now()  
+            #     print("update wamv2 pose")
+            #     self.set_model(model_name = "wamv2", pose = pose_wamv2_to_map)  
+            
 
             # matrix_wamv2_to_origin = np.dot(tf_trans.inverse_matrix(self.matrix_wamv2_origin_to_map), matrix_wamv2_to_map)
             # pose_wamv2_to_origin = self.matrix_to_pose(matrix_wamv2_to_origin, "wamv2")
@@ -181,7 +181,8 @@ class RealtoSimTransform:
         ret_pose_stamped.pose.orientation.z = rot[2]
         ret_pose_stamped.pose.orientation.w = rot[3]
         return ret_pose_stamped
-
+    
+        
 
 if __name__ == "__main__":
     rospy.init_node("real_to_sim_transform")
