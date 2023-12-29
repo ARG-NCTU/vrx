c236a7d285ec2c86a227bd4b69fb5440e8c98214
sunfu-chou <sunfu.chou@gmail.com>
2023-12-29 17:03:55 +0800

    Feat: Add bt integration to track wamv node

--- vrx_gazebo/nodes/mavros_track_wamv_uwb.py
+++ vrx_gazebo/nodes/mavros_track_wamv_uwb.py
@@ -2,11 +2,15 @@
 import numpy as np
 import rospy
 import tf.transformations as tft
+from behavior_tree_msgs.msg import Active, Status
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Bool
 
 
 class MavrosTrackWAMVUWBPose:
+    FAIL = 0
+    RUNNING = 1
+    SUCCESS = 2
     def __init__(self):
         self.trigger_timeout = rospy.get_param("~trigger_timeout", 1.0)
 
@@ -21,8 +25,16 @@ class MavrosTrackWAMVUWBPose:
         self.setpoint_position_local_pub = rospy.Publisher(
             "/mavros/setpoint_position/local", PoseStamped, queue_size=10
         )
+        
+        node_name = rospy.get_name().split("/")[-1]
+        print(node_name)
+
+        self.behavior_active_sub = rospy.Subscriber("/" + node_name + "_active", Active, self.behavior_active_callback)
+        self.behavior_status_pub = rospy.Publisher("/" + node_name + "_status", Status, queue_size=1)
 
         self.timer_pub = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
+
+        self.active = Active()
 
         self.trigger = False
 
@@ -41,12 +53,15 @@ class MavrosTrackWAMVUWBPose:
     def drone_pose_to_local_callback(self, msg):
         self.drone_pose_to_local = self.pose_stamped_to_matrix(msg)
 
+    def behavior_active_callback(self, msg):
+        self.active = msg
+
     def timer_callback(self, event):
         current_time = rospy.Time.now()
-        
+
         if not self.trigger:
             return
-        
+
         if current_time - self.trigger_time > rospy.Duration(self.trigger_timeout):
             self.trigger = False
             return
@@ -59,13 +74,25 @@ class MavrosTrackWAMVUWBPose:
                 wamv_to_local = np.dot(self.drone_pose_to_local, wamv_to_drone)
                 pose_stamped = self.matrix_to_pose_stamped(wamv_to_local, "map")
                 # pose_stamped.pose.position.z = 0.0
-                self.setpoint_position_local_pub.publish(pose_stamped)
+                if self.active.active:
+                    self.setpoint_position_local_pub.publish(pose_stamped)
                 # rospy.loginfo_throttle(1.0, "WAMV to local: \n%s", wamv_to_local)
                 # rospy.loginfo_throttle(1.0, "WAMV to local: \n%s", pose_stamped)
             else:
                 rospy.loginfo_throttle(1.0, "Drone pose to local is not available.")
         else:
             rospy.loginfo_throttle(1.0, "Drone pose to WAMV is not available.")
+        
+        if self.active.active:
+            status = Status()
+            status.id = self.active.id
+            status.status = self.RUNNING
+            self.behavior_status_pub.publish(status)
+        else:
+            status = Status()
+            status.id = self.active.id
+            status.status = self.FAIL
+            self.behavior_status_pub.publish(status)
 
     def pose_stamped_to_matrix(self, pose):
         matrix = tft.quaternion_matrix(
