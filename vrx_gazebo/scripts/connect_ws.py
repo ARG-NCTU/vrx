ed99daff138c650b4192e0f62a30272373a3963b
yutingk <yutingk.ee11@nycu.edu.tw>
2024-01-14 15:07:46 +0800

    pub drone_pose to vr

--- vrx_gazebo/scripts/connect_ws.py
+++ vrx_gazebo/scripts/connect_ws.py
@@ -5,6 +5,7 @@ import rospy
 from geometry_msgs.msg import PoseStamped, Twist
 from sensor_msgs.msg import Joy, LaserScan
 from visualization_msgs.msg import Marker
+from std_msgs.msg import Bool
 # from obstacle_detector.msg import Obstacles
 class ROSBridgeConnector:
     def __init__(self):
@@ -21,6 +22,7 @@ class ROSBridgeConnector:
             # self.pub_obstacle_extractor = roslibpy.Topic(self.client, "/raw_obstacles", "obstacle_detector/Obstacles")
             # pose
             self.pub_wamv_pose = roslibpy.Topic(self.client, "/gazebo/wamv/pose", "geometry_msgs/PoseStamped")
+            self.pub_drone_pose = roslibpy.Topic(self.client, "/drone_pose", "geometry_msgs/PoseStamped")
             
             # pub more_scan and RL scan for obstacle extraction and merge scan
             self.pub_scan =  roslibpy.Topic(self.client, "/wamv/RL/more_scan", "sensor_msgs/LaserScan")
@@ -32,9 +34,11 @@ class ROSBridgeConnector:
             self.sub_scan2_RL = rospy.Subscriber("/wamv2/RL/scan", LaserScan, self.cb_laser_1sub2_RL)
             self.pub_2scan_for1_RL = rospy.Publisher("/wamv2/RL/scan_2", LaserScan, queue_size=1)
             
+            # self.pub_cmd = roslibpy.Topic(self.client, "/wamv2/cmd_vel", "Twist")
+            self.pub_reset = roslibpy.Topic(self.client, "/reset", "Bool")
+            
         elif self.ws == 2:   
             print("ws2") 
-            # self.pub_cmd = roslibpy.Topic(self.client, "/wamv/cmd_vel", "Twist")
             
             # pose            
             self.pub_fake_pose = roslibpy.Topic(self.client, "/fake_fence_real2sim", "geometry_msgs/PoseStamped")
@@ -75,9 +79,12 @@ class ROSBridgeConnector:
             rospy.Subscriber("/gazebo/wamv/pose", PoseStamped, self.cb_wamv_pose)
             rospy.Subscriber("/wamv/RL/more_scan", LaserScan, self.cb_wamv_laser)
             rospy.Subscriber("/wamv/RL/scan", LaserScan, self.cb_wamv_laser_RL)
+            # rospy.Subscriber("/wamv2/cmd_vel", Twist, self.cb_twist)
+            rospy.Subscriber("/reset", Bool, self.cb_reset)
+            rospy.Subscriber("/drone_pose", PoseStamped, self.cb_drone_pose)
+
                         
         elif self.ws == 2:
-            # rospy.Subscriber("/wamv/cmd_vel", Twist, self.cb_twist)
             
             rospy.Subscriber("/fake_fence_real2sim", PoseStamped, self.cb_fake_pose)
             rospy.Subscriber("/gazebo/wamv2/pose", PoseStamped, self.cb_wamv2_pose)
@@ -94,6 +101,14 @@ class ROSBridgeConnector:
         else:
             pass
         
+    def cb_reset(self, data):
+        roslib_msg = roslibpy.Message(
+            {
+                "data": data.data
+            }
+        )
+        self.pub_reset.publish(roslib_msg)
+         
     def cb_laser_1sub2(self, msg):
         laser = msg
         laser.header.stamp = rospy.Time.now()
@@ -193,14 +208,14 @@ class ROSBridgeConnector:
         self.pub_obstacle_extractor.publish(roslib_msg)
 
         
-    def cb_twist(self, data):
-        roslib_msg = roslibpy.Message(
-            {
-                "linear": {"x": data.linear.x, "y": data.linear.y, "z": data.linear.z},
-                "angular": {"x": data.angular.x, "y": data.angular.y, "z": data.angular.z}
-            }
-        )
-        self.pub_cmd.publish(roslib_msg)
+    # def cb_twist(self, data):
+    #     roslib_msg = roslibpy.Message(
+    #         {
+    #             "linear": {"x": data.linear.x, "y": data.linear.y, "z": data.linear.z},
+    #             "angular": {"x": data.angular.x, "y": data.angular.y, "z": data.angular.z}
+    #         }
+    #     )
+    #     self.pub_cmd.publish(roslib_msg)
 
     def cb_joy(self, data, publisher):
         roslib_msg = roslibpy.Message(
@@ -253,7 +268,10 @@ class ROSBridgeConnector:
 
     def cb_wamv_pose(self, data):
         self.cb_posestamped(data, self.pub_wamv_pose)
-
+        
+    def cb_drone_pose(self, data):
+        self.cb_posestamped(data, self.pub_drone_pose)
+        
     def cb_fake_pose(self, data):
         self.cb_posestamped(data, self.pub_fake_pose)
         
