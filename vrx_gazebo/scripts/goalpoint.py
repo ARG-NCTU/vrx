4ae7769dde15c07e62e31a978f3226fbb83a7310
yutingk <yutingk.ee11@nycu.edu.tw>
2024-03-01 19:35:28 +0800

    Feat: modify orientation of goal

--- vrx_gazebo/scripts/goalpoint.py
+++ vrx_gazebo/scripts/goalpoint.py
@@ -120,12 +120,12 @@ class Goal_Point():
             print('wamv2 goal published:', goal_sim_to_map.pose.position.x, goal_sim_to_map.pose.position.y) 
 
         elif self.counter > 15 and self.counter <30:
-            wamv3_goal_to_map = self.goal_to_PoseStamped(self.wamv3_x, self.wamv3_y)
+            wamv3_goal_to_map = self.goal_to_PoseStamped(self.wamv3_x, self.wamv3_y, 0.707, 0.707)
             self.pub_3.publish(wamv3_goal_to_map)
             print('wamv3 goal published:', wamv3_goal_to_map.pose.position.x, wamv3_goal_to_map.pose.position.y)
             
         elif self.counter > 30 and self.counter <45:   
-            wamv4_goal_to_map = self.goal_to_PoseStamped(self.wamv4_x, self.wamv4_y)
+            wamv4_goal_to_map = self.goal_to_PoseStamped(self.wamv4_x, self.wamv4_y, 0.707, 0.707)
             self.pub_4.publish(wamv4_goal_to_map)
             print('wamv4 goal published:', wamv4_goal_to_map.pose.position.x, wamv4_goal_to_map.pose.position.y)
             
